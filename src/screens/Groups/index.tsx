e494656e44c858808d65348aa03f77be198e76e2
Marcelo Pereira <marcelo96jr@gmail.com>
2025-04-27 22:03:43 +0100

    chore: add header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,10 @@
 import { Container } from './styles';
+import { Header } from '@components/Header';
 
 export function Groups() {
-  return <Container>Groups</Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
