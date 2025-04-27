49568821a13289311c2ada3f6c559fe51368068e
Marcelo Pereira <marcelo96jr@gmail.com>
2025-04-27 21:39:59 +0100

    chore: add loading component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import theme from './src/theme';
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
 
@@ -14,7 +16,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
