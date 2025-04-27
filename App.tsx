f93b723d7fbecd8b1b942ffae1ff5408561bf2be
Marcelo Pereira <marcelo96jr@gmail.com>
2025-04-27 19:56:24 +0100

    chore: add theme and theme provider

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
+
 import { Groups } from '@screens/Groups';
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
