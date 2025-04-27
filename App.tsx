200c1c9bff8a97e5e3162a857e127537a2654cbc
Marcelo Pereira <marcelo96jr@gmail.com>
2025-04-27 20:42:55 +0100

    chore: theme config

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
