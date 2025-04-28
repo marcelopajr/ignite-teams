dbaadcdd0661c2319adddde9b37d3760e91e3cc3
Marcelo Pereira <marcelo96jr@gmail.com>
2025-04-28 23:49:59 +0100

    chore: add highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
-import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
+
+import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <Highlight title="Teams" subtitle="Play with your team" />
     </Container>
   );
 }
