836799e1fcce0ef3abc5f7b2706adf1481c264f3
Marcelo Pereira <marcelo96jr@gmail.com>
2025-05-04 21:25:28 +0100

    chore: add list empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { FlatList } from 'react-native';
 import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
@@ -20,6 +21,8 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="No teams found" />}
       />
     </Container>
   );
